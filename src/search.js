c535f4a6284826fa9115747d8024dcc34dab498e
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-08 15:01:25 -0400

    Refactoring of function searchProject

--- src/search.js
+++ src/search.js
@@ -1,16 +1,18 @@
+function isEmptyList(projectList) {
+  return projectList.length === 0;
+}
+
 function searchProject(projectName, projectList) {
-  let searchedProjects = [];
-  if (projectList.length === 0) {
+  if (isEmptyList(projectList)) {
     return "";
   }
-  for (const actualProject in projectList) {
-    if (projectList[actualProject].includes(projectName)) {
-      searchedProjects.push(projectList[actualProject]);
-    }
-  }
-  if (searchedProjects.length === 0) {
+
+  let searchedProjects = projectList.filter(project => project.includes(projectName));
+
+  if (isEmptyList(searchedProjects)) {
     return "";
   }
+
   return searchedProjects.join(", ");
 }
 
